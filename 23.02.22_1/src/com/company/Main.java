f429204496b13102e56e32fe4b000c0583513e7d
Yulia Marchenko <yulia_marchenko@MacBook-Air-Yulia.local>
2022-03-03 10:45:08 +0100

    add hm 25.02 with twoDimArray; task with Vasia and Peter

--- 23.02.22_1/src/com/company/Main.java
+++ 23.02.22_1/src/com/company/Main.java
@@ -12,11 +12,11 @@ public class Main {
         System.out.println(sumArray(array, 0));
 
         // Implement the method that create an array int[n][n] of random integers
-        int[][] NewRandomToDimArray = getNewRandomToDimArray(3, 4, -50, 50);
-        System.out.println(Arrays.deepToString(NewRandomToDimArray));
+        int[][] newRandomToDimArray = getNewRandomToDimArray(3, 4, -50, 50);
+        System.out.println(Arrays.deepToString(newRandomToDimArray));
 
         //Implement the method that finds the row’s index in the array int[n][k] of random integers with minimum sum of elements
-        System.out.println(getIndexMinSum(NewRandomToDimArray));
+        System.out.println(getIndexMinSum(newRandomToDimArray));
     }
 
     public static int[] getNewRandomArray(int size, int min, int max) {
@@ -36,21 +36,21 @@ public class Main {
     }
 
     public static int[][] getNewRandomToDimArray(int rows, int columns, int min, int max) {
-        int[][] NewRandomToDimArray = new int[rows][columns];
+        int[][] newRandomToDimArray = new int[rows][columns];
         Random random = new Random(System.currentTimeMillis());
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                NewRandomToDimArray[i][j] = random.nextInt(max - min + 1) + min;
+                newRandomToDimArray[i][j] = random.nextInt(max - min + 1) + min;
             }
         }
-        return NewRandomToDimArray;
+        return newRandomToDimArray;
     }
 
-    public static int getIndexMinSum(int[][] NewRandomToDimArray) {
+    public static int getIndexMinSum(int[][] newRandomToDimArray) {
         int minSum = Integer.MAX_VALUE;
         int rows = -1;
-        for (int i = 0; i < NewRandomToDimArray.length; i++) {
-            int newSum = sumDimArray(NewRandomToDimArray,i);
+        for (int i = 0; i < newRandomToDimArray.length; i++) {
+            int newSum = sumDimArray(newRandomToDimArray,i);
             if (newSum<minSum){
                 minSum = newSum;
                 rows = i;
@@ -59,10 +59,10 @@ public class Main {
         return rows;
     }
 
-    public static int sumDimArray(int[][] NewRandomToDimArray, int rows) {
+    public static int sumDimArray(int[][] newRandomToDimArray, int rows) {
         int sum = 0;
-        for (int i = 0; i < NewRandomToDimArray[rows].length; i++) {
-            sum += NewRandomToDimArray[rows][i];
+        for (int i = 0; i < newRandomToDimArray[rows].length; i++) {
+            sum += newRandomToDimArray[rows][i];
         }
         return sum;
     }
---
+++ hm_25.02.22/src/com/company/Main.java
@@ -0,0 +1,93 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Найти сумму всех значений n-го подмассива двумерного массива.
+        int[][] twoDimArray = getNewRandomTwoDimArray(3, 4, -50, 50);
+        System.out.println("Sum = " + sumOfNSubArray(twoDimArray, 1));
+        System.out.println();
+
+        // Вывести на экран все значения двумерного массива
+        print2Dim(twoDimArray);
+        System.out.println();
+
+        // Вывести на экран номер строки (подмассива) двумерного массива сумма всех элементов которого равна нулю.
+        int[][] twoDimArrayWithSum0 = {{1, 3, -8, 6}, {2, -2, 1, 5, -1, -5}, {0, 0, 0, 0}};
+        System.out.println(Arrays.deepToString(twoDimArrayWithSum0));
+        System.out.println("Sub array with 0: " + sumOfSubArray0(twoDimArrayWithSum0));
+        System.out.println();
+
+        /* Есть обычный(одномерный) массив целых чисел больше нуля. Вася и Петя задумывают по одному числу.
+        Найти наибольшую длину подмассива этого массива, такого что этот подмассив содержит поровну любимого
+        числа Васи и Пети. Если такого подмассива не существует, вернуть 0. Подмассив имеется в виду часть
+        массива от нулевого элемента и дальше.
+        например: (7, 42)-> {6,42,11,7,1,42}-> 5, (7,42)-> {7,42,11,7,1,42}-> 6 */
+
+        int[] arrNumbers = {7, 42, 11, 7, 1, 42};
+        int numberPeter = 7;
+        int numberVasia = 42;
+        System.out.println("Max length sub array with numbers of Peter and Vasia is " + maxLengthSubArrayWithNumbers(arrNumbers, numberPeter, numberVasia));
+    }
+
+    public static int[][] getNewRandomTwoDimArray(int rows, int columns, int min, int max) {
+        int[][] twoDimArray = new int[rows][columns];
+        Random random = new Random(System.currentTimeMillis());
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                twoDimArray[i][j] = random.nextInt(max - min + 1) + min;
+            }
+        }
+        return twoDimArray;
+    }
+
+    public static int sumOfNSubArray(int[][] twoDimArray, int n) {
+        int sum = 0;
+        for (int i = 0; i < twoDimArray[n].length; i++) {
+            sum += twoDimArray[n][i];
+        }
+        return sum;
+    }
+
+    public static void print2Dim(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(String.format("%d ", array[i][j]));
+            }
+            System.out.println();
+        }
+    }
+
+    public static String sumOfSubArray0(int[][] twoDimArrayWithSum0) {
+        ArrayList<Integer> index = new ArrayList<>();
+        for (int i = 0; i < twoDimArrayWithSum0.length; i++) {
+            int sum = sumOfNSubArray(twoDimArrayWithSum0, i);
+            if (sum == 0) {
+                index.add(i);
+            }
+        }
+        return index.toString();
+    }
+
+    public static int maxLengthSubArrayWithNumbers(int[] arrNumbers, int numberPeter, int numberVasia) {
+        int maxLength = 0;
+        int countPeter = 0;
+        int countVasia = 0;
+        for (int i = 0; i < arrNumbers.length; i++) {
+            if (arrNumbers[i] == numberPeter) {
+                countPeter++;
+            }
+            if (arrNumbers[i] == numberVasia) {
+                countVasia++;
+            }
+            if (countPeter == countVasia) {
+                maxLength = i;
+            }
+        }
+        return maxLength + 1;
+    }
+}
