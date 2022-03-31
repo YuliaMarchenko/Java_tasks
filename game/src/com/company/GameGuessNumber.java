078ae1643c55477a10244ee0f9a8558e8fadcd23
Yulia Marchenko <yulia_marchenko@MacBook-Air-Yulia.local>
2022-03-31 19:30:48 +0200

    game

--- game/src/com/company/GameGuessNumber.java
+++ game/src/com/company/GameGuessNumber.java
@@ -1,2 +1,40 @@
-package com.company;public class GameGuessNumber {
+package com.company;
+
+import java.util.Scanner;
+
+public class GameGuessNumber {
+    private Scanner scanner;
+    private int secret;
+
+    public GameGuessNumber(Scanner scanner, int secret) {
+        this.scanner = scanner;
+        this.secret = secret;
+    }
+
+    public int readNumberOfAttempts(){
+        System.out.println("Enter numbers of attempts");
+        return scanner.nextInt();
+    }
+
+    public int readNumber(){
+        System.out.println("Enter secret number");
+        return scanner.nextInt();
+    }
+
+    public void guessNumber(){
+        int attempts = readNumberOfAttempts();
+        while (attempts >0){
+            int number = readNumber();
+            if (number == secret) {
+                System.out.println("You win!");
+                return;
+            } else {
+                attempts--;
+                System.out.println("Try again");
+            }
+        }
+        System.out.println("You lose");
+    }
 }
+
+
---
+++ game/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+		/* 2.Написать игру Угадай число, в которой за ограниченное число попыток ввода числа
+		с клавиатуры нужно угадать секретное число от 0 до 9
+		Условия:
+		- Код игры должен находиться в классе GameGuessNumber
+		- Класс GameGuessNumber должен содержать две статические переменные scanner для ввода чисел с клавиатуры и secret,
+		в которой записано секретное число
+		- Класс GameGuessNumber должен содержать следующие методы (могут быть и другие по желанию):
+		int readNumberOfAttempts()-считывает количество предоставляемых попыток
+		int readNumber() - считывает вводимое число
+		void guessNumber(int attempts) содержит логику игры. */
+
+        GameGuessNumber game = new GameGuessNumber(new Scanner(System.in),6);
+        game.guessNumber();
+    }
+}
