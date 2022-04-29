e1d15773e5838cb34861ce29a142f0efea0a8380
Yulia Marchenko <yulia_marchenko@MacBook-Air-Yulia.local>
2022-04-29 12:58:29 +0200

    change tests for max element of array

--- maxElementArray/src/com/company/TestMaxElementArray.java
+++ maxElementArray/src/com/company/TestMaxElementArray.java
@@ -8,30 +8,35 @@ public class TestMaxElementArray {
     @Test
     public void max_elemeny_empty_array(){
         int [] array = {};
-        Assertions.assertEquals(Integer.MIN_VALUE, MaxElementArray.maxElementArray(array));
+        int expectedResult = Integer.MIN_VALUE;
+        Assertions.assertEquals(expectedResult, MaxElementArray.maxElementArray(array));
     }
 
     @Test
     public void max_elemeny_last_place(){
         int [] array = {56, 89, 74, 101};
-        Assertions.assertEquals(101, MaxElementArray.maxElementArray(array));
+        int expectedResult = 101;
+        Assertions.assertEquals(expectedResult, MaxElementArray.maxElementArray(array));
     }
 
     @Test
     public void max_elemeny_first_place(){
         int [] array = {560, 89, 74, 101};
-        Assertions.assertEquals(560, MaxElementArray.maxElementArray(array));
+        int expectedResult = 560;
+        Assertions.assertEquals(expectedResult, MaxElementArray.maxElementArray(array));
     }
 
     @Test
     public void max_element_negative_numbers(){
         int [] array = {-560, 809, 74, -101, 78, 90};
-        Assertions.assertEquals(809, MaxElementArray.maxElementArray(array));
+        int expectedResult = 809;
+        Assertions.assertEquals(expectedResult, MaxElementArray.maxElementArray(array));
     }
 
     @Test
     public void max_element_equals_numbers(){
         int [] array = {3, 3, 3, 3, 3, 3, 3, 3};
-        Assertions.assertEquals(3, MaxElementArray.maxElementArray(array));
+        int expectedResult = 3;
+        Assertions.assertEquals(expectedResult, MaxElementArray.maxElementArray(array));
     }
 }
