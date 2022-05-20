8158ed444cc670dd9c6b0a351a8759eb8e6db590
Yulia Marchenko <yulia_marchenko@MacBook-Air-Yulia.local>
2022-05-20 14:16:08 +0200

    change

--- TasksList_20.05.22/src/com/company/MainTest.java
+++ TasksList_20.05.22/src/com/company/MainTest.java
@@ -17,14 +17,14 @@ public class MainTest {
     @Test
     public void testMinPlatforms(){
         List<Integer> arrivals = Arrays.asList(910, 940, 950, 1200, 1500, 1900);
-        List<Integer> departures = Arrays.asList(920, 1200, 1220, 1130, 1800, 2020);
+        List<Integer> departures = Arrays.asList(920, 1200, 1220, 1230, 1800, 2020);
         Assertions.assertEquals(2, Main.minPlatforms(arrivals, departures));
     }
 
     @Test
     public void testMinPlatforms2(){
         List<Integer> arrivals = Arrays.asList(910, 940, 950, 1200, 1500, 1900);
-        List<Integer> departures = Arrays.asList(920, 1200, 1220, 1130, 1800, 2020);
+        List<Integer> departures = Arrays.asList(920, 1200, 1220, 1230, 1800, 2020);
         Assertions.assertEquals(2, Main.minPlatforms2(arrivals, departures));
     }
 }
