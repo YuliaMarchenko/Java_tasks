cb15bef0e0b910681ddb43bab8edaa1120b0f637
Yulia Marchenko <yuyakovleva11@gmail.com>
2022-06-24 12:40:26 +0200

    ...

--- lambdaInterface/src/com/company/Main.java
+++ lambdaInterface/src/com/company/Main.java
@@ -20,23 +20,29 @@ public class Main {
         StringConcate stringConcate = (a, b) -> a + b;
         System.out.println(stringConcate.concate("a", "b"));
         BiFunction<String,String ,String> biFunctionStringConcate = (a, b) -> a + b;
+        System.out.println(biFunctionStringConcate.apply("a","b"));
 
         System.out.println("Task 2 ------------------------");
         Cheсkable check3 = (str) -> str.length()==3;
         System.out.println(check3.check("dog"));
         System.out.println(check3.check("bird"));
         Predicate<String> predicateCheck3 = (str) -> str.length()==3;
+        System.out.println(predicateCheck3.test("dog"));
+        System.out.println(predicateCheck3.test("bird"));
 
         System.out.println("Task 3 ------------------------");
         Cheсkable check4 = (str) -> str.length()==4;
         System.out.println(check4.check("dog"));
         System.out.println(check4.check("bird"));
         Predicate<String> predicateCheck4 = (str) -> str.length()==4;
+        System.out.println(predicateCheck4.test("dog"));
+        System.out.println(predicateCheck4.test("bird"));
 
         System.out.println("Task 4 ------------------------");
         Transformable transformableUpper = (str) -> str.toUpperCase();
         System.out.println(transformableUpper.transformable("dog"));
         Function<String,String> functionUpper = (str) -> str.toUpperCase();
+        System.out.println(functionUpper.apply("dog"));
 
         System.out.println("Task 5 ------------------------");
         Transformable transformableStars = (str) ->
@@ -45,15 +51,19 @@ public class Main {
         System.out.println(transformableStars.transformable("bird"));
         Function<String,String> functionStars = (str) ->
                 str.length()==4?"****":str;
+        System.out.println(functionStars.apply("dog"));
+        System.out.println(functionStars.apply("bird"));
 
         System.out.println("Task 6 ------------------------");
         Printable printable = (str) -> System.out.println("!" + str + "!");
         printable.print("string");
         Consumer<String> consumerStr = (str) -> System.out.println("!" + str + "!");
+        consumerStr.accept("string");
 
         System.out.println("Task 7 ------------------------");
         Producable producable = () -> System.out.println("Hello world");
         producable.result();
         Supplier supplierHelloWorld = () -> "Hello world";
+        System.out.println(supplierHelloWorld.get());
     }
 }
