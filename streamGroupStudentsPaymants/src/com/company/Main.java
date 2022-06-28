284afcdbefd5ab5138a9a52c91439a50d699f9d3
Yulia Marchenko <yuyakovleva11@gmail.com>
2022-06-28 13:42:46 +0200

    hm

---
+++ streamGroupStudentsPaymants/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/* 1. Есть два класса Group {String title, List<Student>}, Student{String name, List<Double> payments}.
+	Необходимо написать метод, который вернет список всех студентов.
+	Учтите, что один и тот же студент может посещать несколько групп.
+
+	2. Есть два класса Group {String title, List<Student>}, Student{String name, List<Double> payments}.
+	Необходимо написать метод, который вернет сумму всех платежей, т.е. общий доход.
+
+	3. Есть два класса Group {String title, List<Student>}, Student{String name, List<Double> payments}.
+	Необходимо написать метод, который вернет студентов, сумма платежей которых меньше заданной суммы.
+
+	4. Используя Stream получить лист c первыми 20 числами Фибоначчи.*/
+    }
+}
